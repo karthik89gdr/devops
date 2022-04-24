ed06e4fd05d3a159fd7dfb90ea9dcdac18039e33
karthik <karthik89gdr@gmail.com>
2022-04-24 11:26:26 +0100

    file

--- new.py
+++ new.py
@@ -1,1 +1,10 @@
+<<<<<<< .merge_file_a21924
 new dev file
+the new file is added for test 
+and many of the things are not so easy
+||||||| .merge_file_a19408
+new dev file
+=======
+the new file is added for test 
+and many of the things are not so easy
+>>>>>>> .merge_file_a15980
